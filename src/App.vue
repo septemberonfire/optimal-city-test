303cae8a770e766e7a3b9183500d2bebeb7f3e27
deadbirdscream <89587459+deadbirdscream@users.noreply.github.com>
2023-12-19 01:42:50 +0300

    checking github actions v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,123 @@
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      messages: [
+        {
+          owner: "bot-message",
+          content: "Привет! Что я могу сделать для вас?",
+          id: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    buyPizza() {
+      this.messages.push({
+        owner: "user-message",
+        content: "Заказать пиццу",
+        id: new Date(),
+      });
 
+      setTimeout(() => {
+        this.scrollChat();
+      }, 10);
+
+      setTimeout(() => {
+        this.messages.push({
+          owner: "bot-message",
+          content: "Хорошо, я закажу для вас пиццу. Что-нибудь ещё?",
+          id: new Date(),
+        });
+        setTimeout(() => {
+          this.scrollChat();
+        }, 10);
+      }, 2000);
+
+      setTimeout(() => {
+        this.scrollChat();
+      }, 10);
+
+
+    },
+    tellAJoke() {
+      this.messages.push({
+        owner: "user-message",
+        content: "Рассказать шутку",
+        id: new Date(),
+      });
+
+      setTimeout(() => {
+        this.scrollChat();
+      }, 10);
+
+      setTimeout(() => {
+        this.messages.push({
+          owner: "bot-message",
+          content: "Хорошо, я расскажу шутку. Что-нибудь ещё?",
+          id: new Date(),
+        });
+        setTimeout(() => {
+          this.scrollChat();
+        }, 10);
+      }, 2000);
+
+      setTimeout(() => {
+        this.scrollChat();
+      }, 10);
+    },
+    showWeather() {
+      this.messages.push({
+        owner: "user-message",
+        content: "Отобразить погоду",
+        id: new Date(),
+      });
+
+      setTimeout(() => {
+        this.scrollChat();
+      }, 10);
+
+      setTimeout(() => {
+        this.messages.push({
+          owner: "bot-message",
+          content:
+            "Хорошо, я отображу погоду на данный момент. Что-нибудь ещё?",
+          id: new Date(),
+        });
+        setTimeout(() => {
+          this.scrollChat();
+        }, 10);
+      }, 2000);
+
+      setTimeout(() => {
+        this.scrollChat();
+      }, 10);
+    },
+
+    scrollChat() {
+      document.querySelector(".dialog").scrollTop = 9999;
+    },
+  },
+};
 </script>
 
 <template>
-  <v-card variant="outlined" class="container">
+  <v-card variant="outlined" 
+  elevation="16"
+  class="container">
     <div class="interlocutor">
-      <img src="./assets/ROBOT.jpg" alt="robot avatar" class="bot-avatar">
+      <img src="./assets/ROBOT.jpg" alt="robot avatar" class="bot-avatar" />
       <h3>Simple VueJS Bot</h3>
     </div>
     <div class="dialog">
-      <div class="bot-message">
-        Hello!
+      <div v-for="el in messages" :key="el.id" :class="el.owner">
+        {{ el.content }}
       </div>
-      <div class="user-message">
-        I want pizza...
-      </div>
-      <div class="bot-message">
-        Got it! Anything else?
-      </div>
+    </div>
+    <div class="btns">
+      <v-btn @click="buyPizza"> Заказать пиццу </v-btn>
+      <v-btn @click="tellAJoke"> Рассказать шутку </v-btn>
+      <v-btn @click="showWeather"> Отобразить погоду </v-btn>
     </div>
   </v-card>
 </template>
@@ -27,8 +127,14 @@
   display: flex;
   flex-direction: column;
   gap: 12px;
-  width: 300px;
+  max-width: 400px;
   padding: 20px;
+  /* -webkit-box-shadow: 10px 10px 26px 7px rgba(34, 60, 80, 0.2);
+  -moz-box-shadow: 10px 10px 26px 7px rgba(34, 60, 80, 0.2);
+  box-shadow: 10px 10px 26px 7px rgba(34, 60, 80, 0.2); */
+}
+
+@media screen and (min-width: 320px) {
 }
 
 .interlocutor {
@@ -39,9 +145,43 @@
 }
 
 .bot-avatar {
-  width: 80px;
+  width: 60px;
   border: 1px solid aquamarine;
   border-radius: 50%;
+}
+
+.dialog {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  border: 1px solid aquamarine;
+  border-radius: 8px;
+  padding: 12px;
+  overflow-y: scroll;
+  height: 200px;
+  scrollbar-width: thin;
+  scroll-behavior: smooth;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  border-radius: 100px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: rgb(13, 235, 235);
+  border-radius: 100px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(9, 196, 196);
 }
 
 .bot-message {
@@ -50,6 +190,8 @@
   border-radius: 8px;
   width: fit-content;
   padding: 12px 16px;
+  max-width: 80%;
+  text-align: left;
 }
 
 .user-message {
@@ -58,5 +200,14 @@
   width: fit-content;
   padding: 12px 16px;
   align-self: flex-end;
+  max-width: 80%;
+  text-align: right;
 }
+
+.btns {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+
 </style>
